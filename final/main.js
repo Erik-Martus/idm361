9d13102e4f3b6e7426222d7f8144d191991289e7
Erik-Martus <43790119+Erik-Martus@users.noreply.github.com>
2019-11-15 19:22:21 -0500

    :sparkles: add new form for dislikes
    Added form for dislikes. Adjusted ids and classes accordingly. Updated CSS and JS.

--- final/main.js
+++ final/main.js
@@ -1,10 +1,25 @@
-var submit_btn = document.getElementById("submit-btn");
+// * handle form switch *
+var like_btn = document.getElementById("toggle-on");
+var dislike_btn = document.getElementById("toggle-off");
+
+var like_sct = document.getElementById("likes");
+var dislike_sct = document.getElementById("dislikes");
+
+like_btn.addEventListener("click", function () {
+  console.log("Clicked likes");
+}, false);
+
+dislike_btn.addEventListener("click", function () {
+  console.log("Clicked dislikes");
+}, false);
+
+// * handle screen state switching *
+var submit_btn = document.getElementById("submit-btn-likes");
 console.log(submit_btn);
 var selection_div = document.getElementById("selection-div");
 var shuffle_div = document.getElementById("shuffling-animation-div");
 var result_div = document.getElementById("result-div");
 
-// var btn_nearby = document.getElementById("nearby");
 var btn_pickAgain = document.getElementById("pick-again");
 
 function playShuffle() {
